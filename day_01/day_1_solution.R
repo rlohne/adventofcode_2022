d70ae903017220ef76cbdd65cd894a0f2b48f6cc
Robert Lohne <rlohne@gmail.com>
2022-12-01 11:59:58 +0100

    simplified the code

--- day_01/day_1_solution.R
+++ day_01/day_1_solution.R
@@ -1,26 +1,21 @@
 # Read the input file
 calories <- scan(file = "day_01/input.txt", blank.lines.skip = FALSE)
 
-# Create a list of the values
-calories_list <- as_tibble(calories) %>%
+# Create a df with the sum of each elf's calories
+calories_df <- as_tibble(calories) %>%
   mutate(value = na_if(value, NA)) %>% 
-  group_by(id_Group =cumsum(is.na(value))+1) %>% 
+  group_by(Group = cumsum(is.na(value)) +1) %>% 
   na.omit() %>% 
-  summarise(value1 =list(value)) %>% 
-  pull(value1)
+  group_by(Group) %>% 
+  summarise(
+    calories = sum(value)
+  )
 
-# Create a data.frame from the list
-dt = rbindlist(
-  lapply(calories_list, function(x) data.table(t(x))),
-  fill = TRUE)
+# Answer the questions
 
-# Sum the list elements for each elf
-df <- dt %>% as_tibble() %>% 
-  mutate(sum = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
-  select(sum)
+## Question 1: 
 
-# Find the maximum amount of calolires one elf is carrying
-max(df$sum)
+max(calories_df$calories)
 
-# Find and sum the top three elves calorie sums 
-sum(head(sort(df$sum, decreasing = TRUE),3))
+## Question 2: 
+sum(head(sort(calories_df$calories, decreasing = TRUE),3))
